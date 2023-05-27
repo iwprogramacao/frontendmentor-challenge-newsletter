ed9a3c4d79ed1282a08817326475552e39a7b4f3
Igor Wiepieski <iw.programacao@gmail.com>
2023-05-27 17:03:45 -0300

    Ajuste de layout entre os elementos principais

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -13,8 +13,8 @@ export default function Home() {
         className="w-full bg-white md:hidden"
       />
       <div className="flex h-full items-center justify-center bg-white md:bg-transparent md:p-4">
-        <div className="flex w-full max-w-[58rem] items-center justify-center gap-16 bg-white px-6 py-10 md:rounded-3xl md:py-6">
-          <div className="max-w-[23.5rem] space-y-6">
+        <div className="flex w-full max-w-[58rem] items-center justify-center bg-white px-6 py-10 md:rounded-3xl md:py-6">
+          <div className="max-w-[28.5rem] space-y-6 p-10">
             <h1 className="text-4xl md:text-title">Stay updated!</h1>
             <p>Join 60,000+ product managers receiving monthly updates on:</p>
             <div className="space-y-2">
